a406bba1a6802b6a406b527647f479fbb9ed56ab
jyosh24j <jyoshnathota24@gmail.com>
2025-08-25 15:41:36 +0530

    Update PrimeNumbers.java

--- PrimeNumbers.java
+++ PrimeNumbers.java
@@ -7,7 +7,7 @@ class PrimeNumbers
        //Empty String
        String  primeNumbers = "";
 
-       for (i = 1; i <= 100; i++)         
+       for (i = 50; i <= 100; i++)         
        { 		  	  
           int counter=0; 	  
           for(num =i; num>=1; num--)
@@ -27,4 +27,5 @@ class PrimeNumbers
        System.out.println(primeNumbers);
    }
 }
+
 
